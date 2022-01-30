1e8eebb8e7d019ebda7db83ed9d98df723334fd7
Bhaskar Kumar <thisisofficialbk@gmail.com>
2022-01-30 22:11:04 +0530

    Steps for using body-parser to tap into  html var

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # BMI Calculator
 Step 1. Setting up Server locally  
 Step 2. Sending html file on server  
-Step 3.
+Step 3. Stay focus on how we are using body-parser to tap into body of html file  
--- bmicalculator.js
+++ bmicalculator.js
@@ -1,14 +1,25 @@
 const express= require("express")
 const app =express();
-
+// body parser
+const bodyParser= require("body-parser")
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",function(req,res){
   res.sendFile(__dirname +"/index.html");
-})
+});
+
+// create post request on server
+app.post("/",function(req,res){
+// console.log(req.body);
+var weight=Number(req.body.weight);
+var height=Number(req.body.Height);
+var result= weight/(height*height);
+res.send("Your Calculated BMI is " + result);
+});
 
 
 
 app.listen(3000,function(req, res){
   console.log("Server Started on port 3000");
-})
+});
